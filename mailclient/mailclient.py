c503a1027fedac14f8066a5be7fa390f99ee2fc2
tientai1312 <150813044+tientai1312@users.noreply.github.com>
2023-11-26 21:55:00 +0700

    client gửi mail tới server

--- mailclient/mailclient.py
+++ mailclient/mailclient.py
@@ -1,7 +1,13 @@
-﻿import base64
-import socket
+﻿import socket
 import os
+import base64
+from datetime import date, datetime
 
+def menu():
+    print("Vui lòng chọn menu: ")
+    print("1. Để gửi email")
+    print("2. Thoát")
+    
 def send_email():
     # Thông tin tài khoản email
     sender_email = input("Nhập địa chỉ email của bạn: ")
@@ -16,24 +22,19 @@ def send_email():
         client_socket.connect((smtp_server, smtp_port))
 
         # Gửi lệnh EHLO để bắt đầu phiên kết nối
-        send_command(client_socket, "EHLO example.com")
-
-            ## Gửi thông tin xác thực
-            #send_command(client_socket, f"AUTH LOGIN")
-            #send_command(client_socket, base64.b64encode(sender_email.encode()).decode())
-            #send_command(client_socket, base64.b64encode(password.encode()).decode())
+        send_command(client_socket, "EHLO [127.0.0.1]")
 
         # Nhập thông tin người nhận
-        to_emails = input("Nhập địa chỉ email người nhận (các địa chỉ cách nhau bởi dấu phẩy): ").split(',')
-        cc_emails = input("Nhập địa chỉ email CC (các địa chỉ cách nhau bởi dấu phẩy): ").split(',')
-        bcc_emails = input("Nhập địa chỉ email BCC (các địa chỉ cách nhau bởi dấu phẩy): ").split(',')
+        to_emails = input("TO: ").split(',')
+        cc_emails = input("CC: ").split(',')
+        bcc_emails = input("BCC: ").split(',')
 
         # Gửi lệnh MAIL FROM
         send_command(client_socket, f"MAIL FROM: <{sender_email}>")
 
         # Gửi lệnh RCPT TO cho người nhận
         for to_email in to_emails:
-            send_command(client_socket, f"RCPT TO: <{to_email}>")  
+            send_command(client_socket, f"RCPT TO: <{to_email}>")
 
         # Gửi lệnh RCPT TO cho CC
         for cc_email in cc_emails:
@@ -44,39 +45,77 @@ def send_email():
             send_command(client_socket, f"RCPT TO: <{bcc_email}>")
 
         # Gửi lệnh DATA để bắt đầu nhập nội dung email
-        send_command(client_socket, "DATA")
+        send_command(client_socket, "DATA") 
 
-        # Nhập nội dung email từ bàn phím
-        print("Nhập nội dung email. Nhấn Enter hai lần để kết thúc.")
-        body_lines = []
+        #Nhập nội dung email từ bàn phím
+        print("Content: ")
+        
+        content_lines = []
         while True:
             line = input()
-            if not line:
+            if not line: 
                 break
-            body_lines.append(line)
-
-        # Gửi nội dung email    
-        body = "\r\n".join(body_lines)
+            content_lines.append(line)
+        body = "\r\n".join(content_lines)
         send_command(client_socket, body)
-
-        # Gửi file đính kèm
-        attach_file_path = input("Nhập đường dẫn đến file đính kèm: ")
-        if os.path.exists(attach_file_path):
-            with open(attach_file_path, 'rb') as attachment:
-                attachment_data = attachment.read()
-                send_command(client_socket, "Content-Type: application/octet-stream")
-                send_command(client_socket, "Content-Transfer-Encoding: binary")
-                send_command(client_socket, f"Content-Disposition: attachment; filename={os.path.basename(attach_file_path)}")
-                send_command(client_socket, "")
-                client_socket.sendall(attachment_data)
-
+        
+        name_attachments = input("Input link attachment: ").split(',')
+        
+        attachments = get_attachment(name_attachments)
+           
+        msg += f"\n\n{attachments}"
+        client_socket.send(attachments)
+        
         # Kết thúc nội dung email
         send_command(client_socket, ".")
+SUPPORTED_FORMATS = {
+    "txt": "text/plain",
+    "doc": "application/msword",
+    "docx": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "pdf": "application/pdf",
+    "jpg": "image/jpeg",
+    "png": "image/png",
+    "xls": "application/vnd.ms-excel",
+    "xlsx": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "zip": "application/zip"
+}
+
+def get_attachment(filename):
+    name = os.path.basename(filename)
+    ext = os.path.splitext(name)[1].lower().strip(".")
+    
+    if ext not in SUPPORTED_FORMATS:
+        return
+        
+    mime_type = SUPPORTED_FORMATS[ext]
+    
+    with open(filename, "rb") as f:
+        data = f.read()
+        
+    b64 = base64.b64encode(data).decode("utf-8")
+    
+    part = f"Content-Type: {mime_type}; name=\"{name}\"\n"
+    part += f"Content-Transfer-Encoding: base64\n" 
+    part += f"Content-Disposition: attachment; filename=\"{name}\"\n\n{b64}"
+    
+    return part
 
 def send_command(socket, command):
     socket.sendall(f"{command}\r\n".encode())
     response = socket.recv(4096).decode()
     print(response)
+    
+def main():
+    while True:
+        menu()
+        choice = int(input("Nhập lựa chọn: "))
+        if choice == 1:
+            send_email()
+            print("Gửi email thành công!")
+        elif choice == 2:   
+              print("Hẹn gặp lại!")
+              exit()
+            
 
-send_email()
-print("Email sent successfully.")
+if __name__ == '__main__':
+    main()
